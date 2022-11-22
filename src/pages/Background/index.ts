783c25fe0b69c6a604d99e892db8c6c7d8ce755e
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-11-22 21:55:48 +1100

    add todo

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,7 +1,3 @@
-// import { ACTIONS } from '../modules/actions';
-// chrome.runtime.onMessage.addListener(function (arg, sender, sendResponse) {
-//   const { type, payload } = arg;
-//   if (type === ACTIONS.SOMETHING) {
-//     sendResponse('');
-//   }
-// });
+// TODO: https://developer.chrome.com/docs/extensions/reference/declarativeNetRequest/
+// This is how manifest 2 does it https://github.com/guilryder/chrome-extensions/blob/main/xframe_ignore/src/manifest.json
+// This is how manifest 3 must do it https://stackoverflow.com/questions/73630289/you-do-not-have-permission-to-use-blocking-webrequest-listeners-be-sure-to-decl
